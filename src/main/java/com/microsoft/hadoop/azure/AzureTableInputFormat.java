ddd296c4dd85d7ecd27eb90183ddd320a0b23777
Mostafa Elhemali <mostafae@microsoft.com>
2013-05-19 20:48:04 -0700

    Initial prototype implementation

---
+++ src/main/java/com/microsoft/hadoop/azure/AzureTableInputFormat.java
@@ -0,0 +1,303 @@
+package com.microsoft.hadoop.azure;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+
+import com.microsoft.windowsazure.services.core.storage.*;
+import com.microsoft.windowsazure.services.table.client.*;
+
+public class AzureTableInputFormat
+		extends InputFormat<String, Map<String, String>> {
+	private static final String TABLE_NAME = "azure.table.name";
+	private static final String ACCOUNT_URI = "azure.table.account.uri";
+	private static final String STORAGE_KEY = "azure.table.storage.key";
+
+	@Override
+	public RecordReader<String, Map<String, String>> createRecordReader(
+			InputSplit split,
+			TaskAttemptContext context)
+					throws IOException, InterruptedException {
+		return new TableRecordReader();
+	}
+
+	@Override
+	public List<InputSplit> getSplits(JobContext context)
+			throws IOException,
+			InterruptedException {
+		Configuration job = context.getConfiguration();
+		CloudTableClient tableClient = createTableClient(job);
+		String tableName = job.get(TABLE_NAME);
+		ArrayList<InputSplit> ret = new ArrayList<InputSplit>();
+		Iterable<String> partitionKeys;
+		try {
+			partitionKeys = getAllPartitionKeys(tableClient.getTableReference(tableName));
+		} catch (URISyntaxException e) {
+			throw new IOException(e);
+		} catch (StorageException e) {
+			throw new IOException(e);
+		}
+		for (String currentPartitionKey : partitionKeys) {
+			ret.add(new PartitionInputSplit(currentPartitionKey));
+		}
+		return ret;
+	}
+	
+	private static DynamicTableEntity GetSingleton(Iterable<DynamicTableEntity> results) {
+		for (DynamicTableEntity t : results) {
+			return t;
+		}
+		return null;
+	}
+	
+	private static TableQuery<DynamicTableEntity> getFirstRowNoFields(CloudTable table) {
+		return TableQuery
+			.from(table.getName(), DynamicTableEntity.class)
+			.select(new String[0])
+			.take(1);
+	}
+
+	static List<String> getAllPartitionKeys(CloudTable table) {
+		TableQuery<DynamicTableEntity> getNextKeyQuery =
+				getFirstRowNoFields(table);
+		CloudTableClient tableClient = table.getServiceClient();
+		TableEntity currentEntity;
+		ArrayList<String> ret = new ArrayList<String>();
+		while ((currentEntity = GetSingleton(tableClient.execute(getNextKeyQuery))) != null) {
+			ret.add(currentEntity.getPartitionKey());
+			getNextKeyQuery = getFirstRowNoFields(table)
+					.where("PartitionKey gt '" + currentEntity.getPartitionKey() + "'");
+		}
+		return ret;
+	}
+
+	private static CloudTableClient createTableClient(Configuration job)
+			throws IOException {
+		String accountUri = job.get(ACCOUNT_URI);
+		String storageKey = job.get(STORAGE_KEY);
+		StorageCredentials creds =
+			new StorageCredentialsAccountAndKey(accountUri, storageKey);
+		CloudTableClient tableClient;
+		try {
+			tableClient =
+				new CloudTableClient(new URI(accountUri), creds);
+		} catch (URISyntaxException e) {
+			throw new IOException(e);
+		}
+		return tableClient;
+	}
+
+	public static class TableRecordReader
+		extends RecordReader<String, Map<String, String>> {
+		private Iterator<DynamicTableEntity> queryResults;
+		private DynamicTableEntity currentEntity;
+
+		/**
+		 * Called once at initialization.
+		 * @param split the split that defines the range of records to read
+		 * @param context the information about the task
+		 * @throws IOException
+		 * @throws InterruptedException
+		 */
+		public void initialize(InputSplit split,
+				TaskAttemptContext context)
+				throws IOException, InterruptedException {
+			String partitionKey = ((PartitionInputSplit)split).getPartitionKey();
+			Configuration job = context.getConfiguration();
+			CloudTableClient tableClient = createTableClient(job);
+			String tableName = job.get(TABLE_NAME);
+			TableQuery<DynamicTableEntity> query =
+					TableQuery
+					.from(tableName, DynamicTableEntity.class)
+					.where("PartitionKey eq '" + partitionKey + "'");
+			queryResults = tableClient.execute(query).iterator();
+		}
+
+		/**
+		 * Read the next key, value pair.
+		 * @return true if a key/value pair was read
+		 * @throws IOException
+		 * @throws InterruptedException
+		 */
+		public boolean nextKeyValue()
+				throws IOException, InterruptedException {
+			if (queryResults.hasNext()) {
+				currentEntity = queryResults.next();
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		/**
+		 * Get the current key
+		 * @return the current key or null if there is no current key
+		 * @throws IOException
+		 * @throws InterruptedException
+		 */
+		public String getCurrentKey()
+				throws IOException, InterruptedException {
+			if (currentEntity == null) {
+				return null;
+			}
+			return currentEntity.getRowKey();
+		}
+		
+		/**
+		 * Get the current value.
+		 * @return the object that was read
+		 * @throws IOException
+		 * @throws InterruptedException
+		 */
+		public Map<String, String> getCurrentValue()
+				throws IOException, InterruptedException {
+			final Map<String, EntityProperty> currentEntityProperties =
+					currentEntity.getProperties();
+			return new Map<String, String>() {
+				@Override
+				public void clear() {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public boolean containsKey(Object key) {
+					return currentEntityProperties.containsKey(key);
+				}
+
+				@Override
+				public boolean containsValue(Object value) {
+					return currentEntityProperties.containsValue(value);
+				}
+
+				@Override
+				public Set<Entry<String, String>> entrySet() { 
+					HashSet<Entry<String, String>> ret =
+							new HashSet<Entry<String, String>>();
+					for (final Entry<String, EntityProperty> curr : currentEntityProperties.entrySet()) {
+						ret.add(new Entry<String, String>() {
+
+							@Override
+							public String getKey() {
+								return curr.getKey();
+							}
+
+							@Override
+							public String getValue() {
+								return curr.getValue().getValueAsString();
+							}
+
+							@Override
+							public String setValue(String value) {
+								throw new UnsupportedOperationException();
+							}
+						});
+					}
+					return ret;
+				}
+
+				@Override
+				public String get(Object key) {
+					return currentEntityProperties.get(key).getValueAsString();
+				}
+
+				@Override
+				public boolean isEmpty() {
+					return currentEntity.getProperties().isEmpty();
+				}
+
+				@Override
+				public Set<String> keySet() {
+					return currentEntity.getProperties().keySet();
+				}
+
+				@Override
+				public String put(String key, String value) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public void putAll(Map<? extends String, ? extends String> m) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public String remove(Object key) {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public int size() {
+					return currentEntity.getProperties().size();
+				}
+
+				@Override
+				public Collection<String> values() {
+					Collection<EntityProperty> wrappedValues =
+							currentEntityProperties.values();
+					ArrayList<String> ret = new ArrayList<String>(wrappedValues.size());
+					for (EntityProperty curr : wrappedValues) {
+						ret.add(curr.getValueAsString());
+					}
+					return ret;
+				}
+			};
+		}
+		
+		/**
+		 * The current progress of the record reader through its data.
+		 * @return a number between 0.0 and 1.0 that is the fraction of the data read
+		 * @throws IOException
+		 * @throws InterruptedException
+		 */
+		public float getProgress()
+				throws IOException, InterruptedException {
+			return 0.5f;
+		}
+		
+		/**
+		 * Close the record reader.
+		 */
+		public void close() throws IOException {
+		}
+	}
+
+	public static class PartitionInputSplit extends InputSplit
+			implements Writable  {
+		private String partitionKey;
+
+		PartitionInputSplit() {}
+
+		public PartitionInputSplit(String partitionKey) {
+			this.partitionKey = partitionKey;
+		}
+		
+		public String getPartitionKey() {
+			return partitionKey;
+		}
+
+		@Override
+		public long getLength() throws IOException, InterruptedException {
+			// TODO No idea how to get the length.
+			return 0;
+		}
+
+		@Override
+		public String[] getLocations() throws IOException, InterruptedException {
+			return new String[] { "localhost" };
+		}
+
+		@Override
+		public void write(DataOutput out) throws IOException {
+			Text.writeString(out, partitionKey);
+		}
+
+		@Override
+		public void readFields(DataInput in) throws IOException {
+			partitionKey = Text.readString(in);
+		}
+	}
+}
---
+++ src/test/java/com/microsoft/hadoop/azure/TestAzureTableInputFormat.java
@@ -0,0 +1,72 @@
+package com.microsoft.hadoop.azure;
+
+import java.net.*;
+import java.util.*;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import com.microsoft.windowsazure.services.core.storage.*;
+import com.microsoft.windowsazure.services.table.client.*;
+
+public class TestAzureTableInputFormat {
+	final String accountUri = "http://mostry.table.core.windows.net/";
+	final String accountKey =
+			"mZyONtAn5w/B+ExPVc+B1ra5d8zMfpTlitpym52nKbJXS4XK6oMcA5LbEh//AiTY0dZXTdsnItMfaXyuuYKQqw==";
+	CloudTable t;
+	
+	@After
+	public void tearDown() throws Exception {
+		if (t != null) {
+			t.delete();
+			t = null;
+		}
+	}
+
+	private static TableEntity newEntity(String partitionKey, String rowKey) {
+		HashMap<String, EntityProperty> properties =
+				new HashMap<String, EntityProperty>();
+		DynamicTableEntity ret = new DynamicTableEntity(properties);
+		ret.setPartitionKey(partitionKey);
+		ret.setRowKey(rowKey);
+		return ret;
+	}
+
+	private static void insertRow(CloudTable t, String partitionKey, String rowKey)
+			throws Exception{
+		t.getServiceClient().execute(t.getName(),
+				TableOperation.insert(newEntity(partitionKey, rowKey)));
+	}
+
+	@Test
+	public void testGetAllPartitionKeys() throws Exception {
+		CloudTableClient tableClient = createTableClient();
+		CloudTable t = createTable(tableClient);
+		insertRow(t, "p1", "r1");
+		insertRow(t, "p1", "r2");
+		insertRow(t, "p2", "r1");
+		insertRow(t, "p3", "r1");
+		List<String> partitionKeys = AzureTableInputFormat.getAllPartitionKeys(t);
+		assertEquals(3, partitionKeys.size());
+		assertEquals("p1", partitionKeys.get(0));
+		assertEquals("p2", partitionKeys.get(1));
+		assertEquals("p3", partitionKeys.get(2));
+	}
+
+	private CloudTable createTable(CloudTableClient tableClient)
+			throws URISyntaxException, StorageException {
+		UUID guid = UUID.randomUUID();
+		String tableName = "t" + guid.toString().replace('-', 'd');
+		CloudTable t = tableClient.getTableReference(tableName);
+		t.create();
+		return t;
+	}
+
+	private CloudTableClient createTableClient() throws URISyntaxException {
+		StorageCredentials creds =
+				new StorageCredentialsAccountAndKey("mostry", accountKey);
+		CloudTableClient tableClient =
+				new CloudTableClient(new URI(accountUri), creds);
+		return tableClient;
+	}
+}
