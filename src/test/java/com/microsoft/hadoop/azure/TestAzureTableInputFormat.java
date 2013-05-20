db0cb41fdb09221c759e912de40e644187b3605c
Mostafa Elhemali <mostafae@microsoft.com>
2013-05-19 21:31:28 -0700

    Get the test account name and key from system property instead of putting them (very securely) in the source (I regenerated the key for the account that was in there)

--- src/test/java/com/microsoft/hadoop/azure/TestAzureTableInputFormat.java
+++ src/test/java/com/microsoft/hadoop/azure/TestAzureTableInputFormat.java
@@ -4,15 +4,15 @@ import java.net.*;
 import java.util.*;
 
 import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+
+import org.apache.hadoop.conf.Configuration;
 import org.junit.*;
 
 import com.microsoft.windowsazure.services.core.storage.*;
 import com.microsoft.windowsazure.services.table.client.*;
 
 public class TestAzureTableInputFormat {
-	final String accountUri = "http://mostry.table.core.windows.net/";
-	final String accountKey =
-			"mZyONtAn5w/B+ExPVc+B1ra5d8zMfpTlitpym52nKbJXS4XK6oMcA5LbEh//AiTY0dZXTdsnItMfaXyuuYKQqw==";
 	CloudTable t;
 	
 	@After
@@ -41,6 +41,7 @@ public class TestAzureTableInputFormat {
 	@Test
 	public void testGetAllPartitionKeys() throws Exception {
 		CloudTableClient tableClient = createTableClient();
+		assumeNotNull(tableClient);
 		t = createTable(tableClient);
 		insertRow(t, "p1", "r1");
 		insertRow(t, "p1", "r2");
@@ -63,10 +64,19 @@ public class TestAzureTableInputFormat {
 	}
 
 	private CloudTableClient createTableClient() throws URISyntaxException {
+		String accountName = System.getProperty("test.account.name");
+		String accountKey = System.getProperty("test.account.key");
+		if (accountName == null || accountKey == null) {
+			System.out.println("Please set the system properties " +
+					"test.account.name and test.account.key.");
+			return null;
+		}
 		StorageCredentials creds =
-				new StorageCredentialsAccountAndKey("mostry", accountKey);
+				new StorageCredentialsAccountAndKey(accountName, accountKey);
 		CloudTableClient tableClient =
-				new CloudTableClient(new URI(accountUri), creds);
+				new CloudTableClient(new URI(String.format(
+						"http://%s.table.core.windows.net",
+						accountName)), creds);
 		return tableClient;
 	}
 }
